6e85ff1702a46fbec7f46de7db6d2f5028c59c38
SellerOfDigitalBalance <faiz.muhammad.rijal.fikri@gmail.com>
2026-06-06 15:40:45 +0700

    feat: implement context menu component with various subcomponents

---
+++ src/components/TheContextMenu.vue
@@ -0,0 +1,148 @@
+<script setup lang="ts">
+import TheSidebar from './TheSidebar.vue';
+import { SidebarProvider } from './ui/sidebar';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { ChevronLeft } from '@lucide/vue'
+import { Button } from './ui/button';
+import { ChevronRight } from '@lucide/vue';
+import { Code2Icon, PlusIcon, TrashIcon } from '@lucide/vue'
+import {
+  ContextMenu,
+  ContextMenuCheckboxItem,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuLabel,
+  ContextMenuRadioGroup,
+  ContextMenuRadioItem,
+  ContextMenuSeparator,
+  ContextMenuShortcut,
+  ContextMenuSub,
+  ContextMenuSubContent,
+  ContextMenuSubTrigger,
+  ContextMenuTrigger,
+} from '@/components/ui/context-menu'
+</script>
+<template>
+  <section class=" px-[3%] relative bg-slate-200/50 dark:bg-black/70 min-h-screen flex items-center justify-center">
+    <SidebarProvider>
+      <TheSidebar class="pt-17" />
+      <div class="pt-18 pr-50">
+        <Card class="border-0 shadow-transparent bg-transparent">
+          <CardHeader>
+            <div class="grid grid-cols-2 gap-4">
+              <div>
+                <CardTitle class="text-4xl text-slate-950 dark:text-white/80">
+                  Context Menu
+                </CardTitle>
+                <CardDescription class="text-sm text-slate-950 dark:text-white/80">
+                  Menampilkan menu kepada pengguna — seperti serangkaian tindakan atau fungsi — yang dipicu oleh klik
+                  kanan.
+                </CardDescription>
+              </div>
+              <div>
+                <CardTitle class="pt-2 text-2xl text-slate-950 dark:text-white/80">
+                  Installation
+                </CardTitle>
+                <CardDescription class="text-sm text-slate-950 dark:text-white/80">
+                  Saya melakukan installation menggunakan npm / npx
+                </CardDescription>
+              </div>
+            </div>
+          </CardHeader>
+          <CardContent>
+            <div class="grid grid-cols-2 gap-4">
+              <div class="border-2 rounded-md px-10 py-10">
+                <ContextMenu>
+                  <ContextMenuTrigger
+                    class="flex h-[150px] w-[300px] items-center justify-center rounded-md border border-dashed text-sm">
+                    Right click here
+                  </ContextMenuTrigger>
+                  <ContextMenuContent class="w-52">
+                    <ContextMenuItem inset class="justify-between">
+                      Back
+                      <ContextMenuShortcut>⌘[</ContextMenuShortcut>
+                    </ContextMenuItem>
+                    <ContextMenuItem inset disabled class="justify-between">
+                      Forward
+                      <ContextMenuShortcut>⌘]</ContextMenuShortcut>
+                    </ContextMenuItem>
+                    <ContextMenuItem inset class="justify-between">
+                      Reload
+                      <ContextMenuShortcut>⌘R</ContextMenuShortcut>
+                    </ContextMenuItem>
+                    <ContextMenuSub>
+                      <ContextMenuSubTrigger inset class="justify-between">
+                        More Tools
+                      </ContextMenuSubTrigger>
+                      <ContextMenuSubContent class="w-44">
+                        <ContextMenuItem inset class="justify-between">
+                          Save Page...
+                          <ContextMenuShortcut>⇧⌘S</ContextMenuShortcut>
+                        </ContextMenuItem>
+                        <ContextMenuItem>
+                          <PlusIcon />
+                          Create Shortcut...
+                        </ContextMenuItem>
+                        <ContextMenuItem inset class="justify-between">
+                          Name Window...
+                        </ContextMenuItem>
+                        <ContextMenuSeparator />
+                        <ContextMenuItem>
+                          <Code2Icon />
+                          Developer Tools
+                        </ContextMenuItem>
+                        <ContextMenuSeparator />
+                        <ContextMenuItem variant="destructive">
+                          <TrashIcon />
+                          Delete
+                        </ContextMenuItem>
+                      </ContextMenuSubContent>
+                    </ContextMenuSub>
+                    <ContextMenuSeparator />
+                    <ContextMenuCheckboxItem :model-value="true" class="justify-between">
+                      Show Bookmarks
+                      <ContextMenuShortcut>⌘⇧B</ContextMenuShortcut>
+                    </ContextMenuCheckboxItem>
+                    <ContextMenuCheckboxItem>Show Full URLs</ContextMenuCheckboxItem>
+                    <ContextMenuSeparator />
+                    <ContextMenuRadioGroup model-value="pedro">
+                      <ContextMenuLabel inset>
+                        People
+                      </ContextMenuLabel>
+                      <ContextMenuRadioItem value="pedro">
+                        Pedro Duarte
+                      </ContextMenuRadioItem>
+                      <ContextMenuRadioItem value="colm">
+                        Colm Tuite
+                      </ContextMenuRadioItem>
+                    </ContextMenuRadioGroup>
+                  </ContextMenuContent>
+                </ContextMenu>
+              </div>
+              <div class="border-2 h-16 rounded-md">
+                <div class="text-center py-4 h-105">
+                  <span>npx shadcn-vue@latest add context-menu</span>
+                </div>
+                <div class="flex justify-end gap-2">
+                  <RouterLink to="/command" class="pr-2">
+                    <Button variant="ghost"
+                      class="cursor-pointer text-slate-800 dark:text-white/85 bg-slate-200/50 dark:bg-white/10 hover:bg-slate-300/50 dark:hover:bg-white/20">
+                      <ChevronLeft class="w-6 h-6 text-white" />Command
+                    </Button>
+                  </RouterLink>
+                  <RouterLink to="/dataTable">
+                    <Button variant="ghost"
+                      class="cursor-pointer text-slate-800 dark:text-white/85 bg-slate-200/50 dark:bg-white/10 hover:bg-slate-300/50 dark:hover:bg-white/20">
+                      Data Table
+                      <ChevronRight class="w-6 h-6 text-white" />
+                    </Button>
+                  </RouterLink>
+                </div>
+              </div>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    </SidebarProvider>
+  </section>
+</template>
